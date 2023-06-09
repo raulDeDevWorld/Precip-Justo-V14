fbd70258673bd557d24e849af8206d1c66ef6b01
raulDeDevWorld <proyection009@gmail.com>
2023-06-09 07:27:54 -0400

    DC

--- src/HOCs/WithAuth.jsx
+++ src/HOCs/WithAuth.jsx
@@ -11,7 +11,7 @@ export function WithAuth(Component) {
     return () => {
         const { user, userDB, setUserProfile, setUserData } = useUser()
         const router = useRouter()
-
+   
         useEffect(() => {
             if(user === undefined)onAuth(setUserProfile)
             if(user === null) router.push('/')
