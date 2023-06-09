6221a0438044f2af5408c9b952ea5d43fd787b8d
raulDeDevWorld <proyection009@gmail.com>
2023-06-09 13:20:38 -0400

    DC

--- src/app/(with-layout)/layout.jsx
+++ src/app/(with-layout)/layout.jsx
@@ -122,7 +122,7 @@ function Home({ children }) {
 
       {user !== undefined && <div className="fixed bottom-0 left-0 z-50 w-full h-[70px] bg-[#0064FA] border-t border-gray-200 rounded-t-[40px]">
         <div className="grid h-full max-w-lg grid-cols-5 mx-auto font-medium">
-          <button type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50  group" onClick={() => redirectHandler(`Distribuidor/Transacciones`)}>
+          <button type="button" class="inline-flex flex-col items-center justify-center px-5 hover:bg-gray-50  group" onClick={() => redirectHandler(`/Distribuidor/Transacciones`)}>
             <svg class="w-9 h-9 mb-1 text-white  group-hover:text-blue-600 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
               <path d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z"></path>
               <path clip-rule="evenodd" fill-rule="evenodd" d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z"></path>
